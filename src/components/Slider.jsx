8de411ee3c81ad52c4280536dc0ce513b5f5a346
Evgenij_B <evgenij.baltais@gmail.com>
2022-01-18 01:55:34 +0300

    1

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import '../styles/mainSlider.css'
 
 import slider_pic_1 from '../images/1.jpg'
@@ -6,16 +6,37 @@ import slider_pic_2 from '../images/2.jpg'
 
 const Slider = () => {
 
-    const [slides, updateSlides] = useState(2)
-    const [time, updateTime] = useState(2000)
+    const slides = [1, 2, 3]
+    const time = 2000
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+
+            setActiveIndex(current => {
+
+                const res = current === slides.length - 1 ? 0 : current + 1
+                return res
+            })
+        }, time)
+        return () => clearInterval() 
+    }, [])
 
     return (
 
         <div className = "main-slider">
+            <div className = "slider-pics">
+
+                {
+                    slides.map(number => 
+                        <div className = 'slider-pic' key = {number}></div>
+                    )
+                }
+
+            </div>
             <div className="left-arrow"></div>
             <div className="right-arrow"></div>
-            <div className="slider-pic"></div>
-            <div className="slider-pic"></div>
         </div>
     )
 }
